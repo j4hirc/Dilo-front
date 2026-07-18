2f87a6af4be8ea03195d6bafa0e268e3e53a75b5
j4hirc <112521047+j4hirc@users.noreply.github.com>
2026-07-17 22:45:24 -0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+// Importamos tu componente Home (asegúrate de que el nombre de la clase sea el correcto)
+import { Home } from './home/home'; 
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home }, // Esta es la ruta raíz (tu Landing Page)
+  
+  // Estas las descomentaremos cuando crees los componentes
+  // { path: 'login', component: LoginComponent }, 
+  // { path: 'registro', component: RegistroComponent },
+  
+  { path: '**', redirectTo: '' } // Si alguien pone una URL rara, lo regresa al inicio
+];
